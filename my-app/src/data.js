048b51951bc2c2adede20fa9acadfd970f8c89f2
Tin Nguyen <nguyenbatin290900@gmail.com>
2023-03-29 10:13:44 +0700

    Update file data.js

---
+++ my-app/src/data.js
@@ -0,0 +1,22 @@
+const data = {
+    user: [
+        {
+            user: 'anpham',
+            password: '123456',
+            gmail: 'phanan@gmail.com',
+            fullname: 'Pham Thien An',
+            address: 'Buon Ma Thuot',
+            date: '23/10/2001',
+            phone: '0357894532',
+        },
+        {
+            user: 'quanghoa',
+            password: '123456',
+            gmail: 'quanghoa@gmail.com',
+            fullname: 'Quang Hoa',
+            address: 'Phu cat',
+            date: '23/10/2001',
+            phone: '0357894532',
+        }
+    ]
+}
